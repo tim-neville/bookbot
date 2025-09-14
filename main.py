76a38d2c1c877fdce4e647cc54f28d7d6c4a7782
tim-neville <nikefootbag@gmail.com>
2025-09-15 07:39:41 +1000

    Start bookbot report generation

--- main.py
+++ main.py
@@ -6,15 +6,30 @@ def get_book_text(filepath):
         file_contents = f.read()
     return file_contents
 
+def sort_on(items):
+    return items["count"]
+
 def main():
-    file_contents = get_book_text("books/frankenstein.txt")
+    print("============ BOOKBOT ============")
+    filepath="books/frankenstein.txt"
+    print(f"Analyzing book found at {filepath}...")
+    file_contents = get_book_text(filepath)
+
+    print("----------- Word Count ----------")
     word_count = count_words(file_contents)
-    print(f"{word_count} words found in the document")
-    
+    print(f"Found {word_count} total words")
+
+    print("--------- Character Count -------")
     character_counts = count_characters(file_contents)
+
+    #double dictionary char: value and count: value
+
+    # character_counts.sort(reverse=True, key=sort_on)
 
     for key in character_counts:
         value = character_counts[key]
         print(f"'{key}': {value}")
+
+    print("============= END ===============")
 
 main()
